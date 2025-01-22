c4af891d0a100cc53b3a73b93f47ee16a8e5e2a4
everett <everett.snieder@gmail.com>
2025-01-22 13:37:45 -0700

    bugfix1

--- StormReactor/__init__.py
+++ StormReactor/__init__.py
@@ -1,6 +1,7 @@
 # Import class from package
 from StormReactor.waterQualityConfig import WQConfig
 from StormReactor.waterQuality import waterQuality, WQStorage
+from StormReactor.defs import ElementType
 
 __version__ = "1.4.0"
 
